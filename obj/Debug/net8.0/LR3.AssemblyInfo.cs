bdc1fe8e2aa36b74d9d9ce530a96826024705816
Владислав Максаков <vladislavmaksakov@MacBook-Air-Vladislav.local>
2026-01-21 15:41:23 +0200

    Update ReadMe

--- obj/Debug/net8.0/LR3.AssemblyInfo.cs
+++ obj/Debug/net8.0/LR3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LR3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbd2f91089663226c677ff14f265e09b1dea2a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+494f39cdc5b7ea810472bcffd7b96778e0474ccf")]
 [assembly: System.Reflection.AssemblyProductAttribute("LR3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LR3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
